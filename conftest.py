d35d883d3389b9d25265bfc4f67f2098f77bf45d
Damir Sabitov <damirsabitov1998@gmail.com>
2025-09-22 02:02:12 +0300

    fix conftest

--- conftest.py
+++ conftest.py
@@ -2,10 +2,13 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
+from tempfile import mkdtemp
 
 @pytest.fixture(scope="function")
 def setup_browser():
     options = Options()
+    options.add_argument(f"--user-data-dir={mkdtemp()}")  # уникальная папка профиля
+
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "128.0",
@@ -15,10 +18,12 @@ def setup_browser():
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
     driver = webdriver.Remote(
         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
+
     browser = Browser(Config(driver))
     yield browser
     browser.quit()
