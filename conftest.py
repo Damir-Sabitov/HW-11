e1383f77c500e28f790bd4f0d55560f31d63bc1d
Damir Sabitov <damirsabitov1998@gmail.com>
2025-09-22 00:43:40 +0300

    fix fixture 2

--- conftest.py
+++ conftest.py
@@ -1,14 +1,28 @@
 import pytest
+import tempfile
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 from utils import attach
 
+SELENOID_URL = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
-    selenoid_capabilities = {
+    # Создаём временную уникальную папку для user-data-dir
+    user_data_dir = tempfile.mkdtemp()
+
+    options = Options()
+    options.add_argument(f"--user-data-dir={user_data_dir}")  # уникальный профиль
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--remote-allow-origins=*")
+
+    # Selenoid capabilities
+    capabilities = {
         "browserName": "chrome",
-        "browserVersion": "latest",   # всегда актуальная версия
+        "browserVersion": "latest",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
@@ -16,14 +30,15 @@ def setup_browser(request):
     }
 
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        desired_capabilities=selenoid_capabilities
+        command_executor=SELENOID_URL,
+        options=options,
+        desired_capabilities=capabilities
     )
 
     browser = Browser(Config(driver))
     yield browser
 
-    # фиксация артефактов в Allure
+    # Сбор артефактов
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
